a14fc3e27bf998b331baea84b4246d7540a9b627
smartsri18 <sridharan479@gmail.com>
2020-11-30 20:21:15 +0530

    code updated

--- web_scrape.rb
+++ web_scrape.rb
@@ -1,13 +1,18 @@
 require 'rest-client'
 
-response = RestClient.get 'https://time.com/'
+def fetch_news
+  response = RestClient.get 'https://time.com/'
+  response.body
+end
 
-body = response.body
+news = fetch_news
 # body.gsub!(/\n\s+/, " ")
 
+# Latest stories section
 latest_stories_section_regex = /<section class="homepage-module latest" data-module_name="Latest Stories">((.|\n)*?)<\/section>/
-latest_stories_section = body.match(latest_stories_section_regex)[0]
+latest_stories_section = news.match(latest_stories_section_regex)[0]
 
+# Stories
 stories_regex = /<li>((.|\n)*?)<\/li>/
 stories = latest_stories_section.scan(stories_regex).flatten
 stories.reject! { |e| e == " " }
@@ -19,7 +24,7 @@ story_href_regex = /href=((.)*)\/>/
 story_title_regex = /\/>((.)*)<\/a>/
 stories.each do |story, stories_hash={} |
   stories_hash[:title] = story.match(story_title_regex)[1]
-  stories_hash[:href] = "https://time.com/#{story.match(story_href_regex)[1]}"
+  stories_hash[:href] = "https://time.com#{story.match(story_href_regex)[1]}"
   stories_list.push(stories_hash)
 end
 
